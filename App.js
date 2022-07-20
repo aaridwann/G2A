bdd40d5b111d071fb2edba2129b3fea86fb4d402
Ridwan Firmansyah <aaridwann@gmail.com>
2022-07-20 20:54:48 +0700

    fixing refresh video when replace detail

--- App.js
+++ App.js
@@ -1,18 +1,13 @@
-import React, { Suspense } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './src/Screen/Home';
 import Details from './src/Screen/Details';
-import TabBar from './src/Tab/TabBar';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 const Tab = createBottomTabNavigator();
 import { Icon } from 'react-native-elements'
-import Profile from './src/Screen/Profile';
 
 
-const Stack = createNativeStackNavigator();
 export default function App() {
   return (
     <NavigationContainer>
